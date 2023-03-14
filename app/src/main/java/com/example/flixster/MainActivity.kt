0260a7d07a261cee4ec420a6d6e15b1d7561e5ba
mukhsadr <mukhsadr@gmail.com>
2023-03-14 15:46:04 -0500

    feat: formatted MainActivity.kt for better readability

--- app/src/main/java/com/example/flixster/MainActivity.kt
+++ app/src/main/java/com/example/flixster/MainActivity.kt
@@ -1,43 +1,41 @@
 package com.example.flixster
 
+
+import com.example.flixster.Fragments.NowPlaying
+import com.example.flixster.Fragments.NowTrending
+import com.example.flixster.Fragments.NowUpcoming
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import com.example.flixster.Fragments.NowPlaying
-import com.example.flixster.Fragments.NowTrending
-import com.example.flixster.Fragments.NowUpcoming
+
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var fragmentManager: FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // set up Fragment Manager and Bottom Navigation Bar;
-        val fragmentManager: FragmentManager = supportFragmentManager
-        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener(){
-            item ->
-            var fragmentToShow: Fragment? = null
-            when(item.itemId){
-                R.id.action_play -> {
-                    fragmentToShow = NowPlaying()
-                }
-                R.id.action_trend -> {
-                    fragmentToShow = NowTrending()
-                }
-                R.id.action_upcome -> {
-                    fragmentToShow = NowUpcoming()
-                }
+        // Set up Fragment Manager and Bottom Navigation Bar
+        fragmentManager = supportFragmentManager
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            val fragmentToShow: Fragment = when (item.itemId) {
+                R.id.action_play -> NowPlaying()
+                R.id.action_trend -> NowTrending()
+                R.id.action_upcome -> NowUpcoming()
+                else -> NowPlaying()
             }
-            if (fragmentToShow != null){
-                fragmentManager.beginTransaction().replace(R.id.flContainer, fragmentToShow).commit()
-            }
+            fragmentManager.beginTransaction()
+                .replace(R.id.flContainer, fragmentToShow)
+                .commit()
             true
         }
-        // set default value;
-        findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.action_play
+
+        // Set default value
+        bottomNavigationView.selectedItemId = R.id.action_play
     }
 }
