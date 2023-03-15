3b7d368dc686100df44312fd05051c41cdc48a82
mukhsadr <mukhsadr@gmail.com>
2023-03-14 20:18:40 -0500

    style: improved movie detail layout.

---
+++ app/src/main/java/com/example/flixster/Fragments/BaseMovieFragment.kt
@@ -0,0 +1,85 @@
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.example.flixster.Movie
+import com.example.flixster.MovieAdapter
+import com.example.flixster.R
+import okhttp3.Headers
+import org.json.JSONException
+
+open class BaseMovieFragment(private val apiUrl: String) : Fragment() {
+
+    private val movies = mutableListOf<Movie>()
+    private lateinit var rvMovies: RecyclerView
+    private lateinit var movieAdapter: MovieAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_base_movie, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rvMovies = view.findViewById(R.id.rvMovies)
+        movieAdapter = MovieAdapter(requireContext(), movies)
+        rvMovies.adapter = movieAdapter
+        rvMovies.layoutManager = LinearLayoutManager(requireContext())
+        val client = AsyncHttpClient()
+        client.get(apiUrl, object : JsonHttpResponseHandler(){
+            override fun onFailure(statusCode: Int, headers: Headers?, response: String?, throwable: Throwable?
+            ) {
+                Log.e(TAG, "onFailure.")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                try {
+                    Log.i(TAG, "onSuccess. $statusCode")
+                    val movieJsonArray = json.jsonObject.getJSONArray("results")
+                    movies.addAll(Movie.fromJsonArray(movieJsonArray))
+                    movieAdapter.notifyDataSetChanged()
+                }
+                catch (e: JSONException) { Log.e(TAG, "Encountered exception $e.") }
+            }
+
+        })
+        view.findViewById<Button>(R.id.btn_search).setOnClickListener(){
+            val text = view.findViewById<EditText>(R.id.et_search).text.toString()
+            val URL = "https://api.themoviedb.org/3/search/movie?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&query=" + text + "&page=1&include_adult=false"
+            client.get(URL, object : JsonHttpResponseHandler(){
+                override fun onFailure(statusCode: Int, headers: Headers?, response: String?, throwable: Throwable?
+                ) {
+                    Log.e(TAG, "onFailure.")
+                }
+
+                override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                    try {
+                        Log.i(TAG, "onSuccess. $statusCode")
+                        val movieJsonArray = json.jsonObject.getJSONArray("results")
+                        movies.clear()
+                        movies.addAll(Movie.fromJsonArray(movieJsonArray))
+                        movieAdapter.notifyDataSetChanged()
+                    }
+                    catch (e: JSONException) { Log.e(TAG, "Encountered exception $e.") }
+                }
+
+            })
+        }
+    }
+
+    companion object {
+        private const val TAG = "BaseMovieFragment"
+    }
+}
